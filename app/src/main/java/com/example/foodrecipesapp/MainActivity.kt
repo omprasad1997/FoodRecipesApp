85027b89470178c155aa4988015cfd187d965cd7
omprasad1997 <20rathodomprasad@gmail.com>
2022-11-16 00:34:59 +0530

    Created 3 fragment for recipes, favourite recipes, food joke, Navigation Host Fragment, Bottom Navigation View

--- app/src/main/java/com/example/foodrecipesapp/MainActivity.kt
+++ app/src/main/java/com/example/foodrecipesapp/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.foodrecipesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController:NavController
+    private lateinit var bottomNavView:BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavView = findViewById(R.id.bottomNavigationView)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favouriteRecipesFragment,
+                R.id.foodJokeFragment
+            )
+        )
+
+        bottomNavView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
